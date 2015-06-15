93e57ceec4224bb9a1633a32b61de5729cf3db5b
Clinton Dreisbach <clinton@dreisbach.us>
2015-06-15 15:12:10 -0400

    Added django debug toolbar and extensions

--- mowdie/mowdie/settings.py
+++ mowdie/mowdie/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django_extensions',
     'updates',
     'users',
 )
